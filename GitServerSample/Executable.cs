84ee47cd07f81acaf1e93f41d77ce49fd484cf74
Suwath Ch <suwatch@microsoft.com>
2016-01-27 01:51:58 -0800

    initial commit

---
+++ GitServerSample/Executable.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GitServerSample
+{
+    public class Executable
+    {
+        public Executable()
+        {
+            Path = Program.GitExe;
+            WorkingDirectory = Program.RepositoryPath;
+        }
+
+        public string WorkingDirectory { get; private set; }
+
+        public string Path { get; private set; }
+
+        // This is pure async process execution
+        public async Task<int> ExecuteAsync(string arguments, Stream output, Stream input = null)
+        {
+            using (Process process = CreateProcess(arguments))
+            {
+                return await Start(process, output, input);
+            }
+        }
+        public static async Task<int> Start(Process process, Stream output, Stream input = null)
+        {
+            var cancellationTokenSource = new CancellationTokenSource();
+
+            process.Start();
+
+            var tasks = new List<Task>();
+
+            if (input != null)
+            {
+                tasks.Add(CopyStreamAsync(input, process.StandardInput.BaseStream, cancellationTokenSource.Token, closeAfterCopy: true));
+            }
+
+            var error = new MemoryStream();
+            tasks.Add(CopyStreamAsync(process.StandardOutput.BaseStream, output, cancellationTokenSource.Token));
+            tasks.Add(CopyStreamAsync(process.StandardError.BaseStream, error, cancellationTokenSource.Token));
+
+            process.WaitForExit();
+
+            await Task.WhenAll(tasks);
+
+            return process.ExitCode;
+        }
+
+        private static async Task CopyStreamAsync(Stream from, Stream to, CancellationToken cancellationToken, bool closeAfterCopy = false)
+        {
+            try
+            {
+                byte[] bytes = new byte[1024];
+                int read = 0;
+                while ((read = await from.ReadAsync(bytes, 0, bytes.Length, cancellationToken)) != 0)
+                {
+                    await to.WriteAsync(bytes, 0, read, cancellationToken);
+                }
+            }
+            finally
+            {
+                // this is needed specifically for input stream
+                // in order to tell executable that the input is done
+                if (closeAfterCopy)
+                {
+                    to.Close();
+                }
+            }
+        }
+
+        internal Process CreateProcess(string arguments)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = Path,
+                WorkingDirectory = WorkingDirectory,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                UseShellExecute = false,
+                ErrorDialog = false,
+                Arguments = arguments
+            };
+
+            psi.StandardOutputEncoding = Encoding.UTF8;
+            psi.StandardErrorEncoding = Encoding.UTF8;
+
+            return new Process()
+            {
+                StartInfo = psi
+            };
+        }
+    }
+}
---
+++ GitServerSample/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace GitServerSample
+{
+    static class Program
+    {
+        internal static string GitExe = @"C:\Program Files (x86)\Git\cmd\git.exe";
+        internal static string RepositoryPath;
+
+        // initialize git repo as follow
+        // > mkdir repository
+        // > cd repository
+        // > git init .
+        // > git config receive.denyCurrentBranch ignore
+        static void Main(string[] args)
+        {
+            try
+            {
+                RepositoryPath = args[0];
+                var port = Int32.Parse(args[1]);
+                string prefix = String.Format("http://*:{0}/", port);
+                var listener = new HttpListener();
+                listener.Prefixes.Add(prefix);
+                listener.Start();
+                Console.WriteLine("Listening at " + prefix);
+                while (true)
+                {
+                    // simple handle one request at a time
+                    ProcessRequest(listener.GetContext());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        static void ProcessRequest(HttpListenerContext context)
+        {
+            Console.WriteLine("{0} {1} {2}", DateTime.UtcNow.ToString("o"), context.Request.HttpMethod, context.Request.Url.PathAndQuery);
+
+            var service = GetService(context.Request.Url.PathAndQuery);
+            context.Response.Headers["Content-Type"] = String.Format("application/x-git-{0}-advertisement", service);
+            context.Response.Headers["Expires"] = "Fri, 01 Jan 1980 00:00:00 GMT";
+            context.Response.Headers["Pragma"] = "no-cache";
+            context.Response.Headers["Cache-Control"] = "no-cache, max-age=0, must-revalidate";
+
+            if (context.Request.HttpMethod == "GET" && context.Request.Url.AbsolutePath == "/info/refs")
+            {
+                var input = String.Format("# service=git-{0}\n", service);
+                var toWrite = (input.Length + 4).ToString("x").PadLeft(4, '0') + input;
+                context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(toWrite), 0, Encoding.UTF8.GetByteCount(toWrite));
+
+                toWrite = "0000";
+                context.Response.OutputStream.Write(Encoding.UTF8.GetBytes(toWrite), 0, Encoding.UTF8.GetByteCount(toWrite));
+
+                var exe = new Executable();
+                var args = String.Format("{0} --stateless-rpc --advertise-refs \"{1}\"", service, Program.RepositoryPath);
+                exe.ExecuteAsync(args, context.Response.OutputStream).Wait();
+                context.Response.Close();
+            }
+            else
+            {
+                var exe = new Executable();
+                var args = String.Format("{0} --stateless-rpc \"{1}\"", service, Program.RepositoryPath);
+                exe.ExecuteAsync(args, context.Response.OutputStream, GetInputStream(context.Request)).Wait();
+                context.Response.Close();
+            }
+        }
+
+        static Stream GetInputStream(HttpListenerRequest request)
+        {
+            var contentEncoding = request.Headers["Content-Encoding"];
+
+            if (contentEncoding != null && contentEncoding.Contains("gzip"))
+            {
+                return new GZipStream(request.InputStream, CompressionMode.Decompress);
+            }
+
+            return request.InputStream;
+        }
+
+        static string GetService(string pathAndQuery)
+        {
+            return pathAndQuery.Substring(pathAndQuery.IndexOf("git-") + "git-".Length);
+        }
+    }
+}
